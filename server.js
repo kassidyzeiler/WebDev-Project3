f09b11e73b774903773b694e8182945d37fc8b37
Kassidy Zeiler <knzeiler@ksu.edu>
2019-06-30 20:17:02 +0000

    static fileserver

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var http = require('http');
+var requestHandler = require('./request-handle');
+var port = 3000;
+
+var server = http.createServer(requestHandler);
+
+server.listen(port, function(){
+  console.log("Server is listening on port " + port);
+});
